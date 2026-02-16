4cc4c45f8f96e87001085250fa5711b3c216a71f
Nikita Satish Madke <madkenikita35@gmail.com>
2026-02-16 13:47:09 +0530

    Add Styling to the Product

--- ECommerceReact/src/App.jsx
+++ ECommerceReact/src/App.jsx
@@ -1,1 +1,18 @@
+import React from "react";
+import { Router, Route, Routes } from "react-router-dom";
 
+import Home from "./Component/Home";
+import Products from "./Component/Products";
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ ECommerceReact/src/Component/AddProduct.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useState } from "react";
+
+const AddProduct = () => {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div className="inline-flex items-center justify-center gap-2 bg-gray-300 border-2 border-gray-300 rounded-lg">
+      <button onClick={() => setCounter(counter + 1)}>+{counter}</button>
+      <button onClick={() => setCounter(counter - 1)}>-</button>
+    </div>
+  );
+};
+
+export default AddProduct;
