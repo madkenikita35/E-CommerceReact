56a15ee622fb4656c184942d341e1199175b0bc0
Nikita Satish Madke <madkenikita35@gmail.com>
2026-02-16 15:25:07 +0530

    Add

--- ECommerceReact/src/Component/AddProduct.jsx
+++ ECommerceReact/src/Component/AddProduct.jsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 
 const AddProduct = () => {
   const [counter, setCounter] = useState(0);
+  if (counter < 0) {
+    setCounter(0);
+  }
   return (
     <div className="inline-flex items-center justify-center gap-2 bg-amber-300 border-2 border-gray-300 rounded-lg">
       <button onClick={() => setCounter(counter + 1)}>+{counter}</button>
