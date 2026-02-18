96edd3b1bacd4f955b61f15d66dfae7a049e673a
Nikita Satish Madke <madkenikita35@gmail.com>
2026-02-18 09:11:03 +0530

    Add Changes

---
+++ ECommerceReact/src/Component/Headline.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../src/marquee.css";
+
+const Headline = () => {
+  return (
+    <div className="overflow-hidden bg-orange-400">
+      <p className="marquee text-2xl shine-text">
+        Flat 30% OFF – Limited Time Offer!
+      </p>
+    </div>
+  );
+};
+
+export default Headline;
--- ECommerceReact/src/Component/Home.jsx
+++ ECommerceReact/src/Component/Home.jsx
@@ -1,138 +1,3 @@
-// // import React from "react";
-// // import { Link } from "react-router-dom";
-// // import Header from "./Header";
-// // import bg from "../assets/bg.png";
-// // import NavBar from "./NavBar";
-// // import Nave from "./Navigation/Nav";
-// // import Products from "./Products/Products";
-// // import Recommendation from "../Recommendation/Recommendation";
-// // import Sidebar from "../Sidebar/Sidebar";
-// // import {useState} from "react";
-// // import { FiAlignJustify } from "react-icons/fi";
-
-// // const Home = () => {
-// //   const [menu, setMneu] = useState[false];
-
-// //   const setMenuF = () =>{
-// //     setMneu(true);
-// //   }
-// //   return
-// //   (
-// //   <>
-// //     {menu ? (
-// //       <div>
-// //             <Sidebar />
-// //             <div className="ml-[20%]">
-// //               <Header />
-// //               <NavBar />
-// //               <div className="relative">
-// //                 <img src={bg} alt="Banner" className="w-full h-96 object-cover" />
-// //                 <div className="absolute inset-0 flex items-center justify-center">
-// //                   <h1 className="text-4xl font-bold text-white">
-// //                     Welcome to Our E-Commerce Store
-// //                   </h1>
-// //                 </div>
-// //               </div>
-// //               <Nave />
-// //               <Recommendation />
-// //               <Products />
-// //             </div>
-// //           </div> ) :   (
-// //             <div>
-// //           <button onClick={setMenuF}>
-// //             <FiAlignJustify />
-// //           </button>
-
-// //         </div>
-// //       )}
-// //   </>
-// // );
-// // };
-// // export default Home;
-
-// import React, { useState } from "react";
-// import Header from "./Header";
-// import bg from "../assets/bg.png";
-// import NavBar from "./NavBar";
-// import Nave from "./Navigation/Nav";
-// import Products from "./Products/Products";
-// import Recommendation from "../Recommendation/Recommendation";
-// import Sidebar from "../Sidebar/Sidebar";
-// import { FiAlignJustify } from "react-icons/fi";
-
-// const Home = () => {
-//   const [menu, setMenu] = useState(false);
-
-//   const setMenuF = () => {
-//     setMenu(true);
-//   };
-
-//   return (
-//     <>
-//       {menu ? (
-//         <div>
-//           <Sidebar />
-//           <div className="ml-[20%]">
-//             <Header />
-//             <NavBar />
-//             <div className="relative">
-//               <img src={bg} alt="Banner" className="w-full h-96 object-cover" />
-//               <div className="absolute inset-0 flex items-center justify-center">
-//                 <h1 className="text-4xl font-bold text-white">
-//                   Welcome to Our E-Commerce Store
-//                 </h1>
-//               </div>
-//             </div>
-//             <Nave />
-//             <Recommendation />
-//             <Products />
-//           </div>
-//         </div>
-//       ) : (
-//         <div>
-//           {/* <button onClick={setMenuF}>
-//             <FiAlignJustify />
-//           </button> */}
-//           <div>
-//             <div>
-//               <Header />
-//               <>
-//                 <button className="inline-block w-5" onClick={setMenuF}>
-//                   <FiAlignJustify />
-//                 </button>
-//                 <p className="inline">
-//                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
-//                   Obcaecati, ipsam corrupti nulla debitis corporis laudantium
-//                   aliquid reiciendis fugit sit saepe minus perspiciatis facilis,
-//                   ratione autem quae.
-//                 </p>
-//               </>
-//               {/* <NavBar /> */}
-//               <div className="relative">
-//                 <img
-//                   src={bg}
-//                   alt="Banner"
-//                   className="w-full h-96 object-cover"
-//                 />
-//                 <div className="absolute inset-0 flex items-center justify-center">
-//                   <h1 className="text-4xl font-bold text-white">
-//                     Welcome to Our E-Commerce Store
-//                   </h1>
-//                 </div>
-//               </div>
-//               <Nave />
-//               <Recommendation />
-//               <Products />
-//             </div>
-//           </div>
-//         </div>
-//       )}
-//     </>
-//   );
-// };
-
-// export default Home;
-
 import React, { useState } from "react";
 import Header from "./Header";
 import bg from "../assets/bg.png";
@@ -142,6 +7,7 @@ import Products from "./Products/Products";
 import Recommendation from "../Recommendation/Recommendation";
 import Sidebar from "../Sidebar/Sidebar";
 import { FiAlignJustify } from "react-icons/fi";
+import Headline from "./Headline";
 
 const Home = () => {
   const [menu, setMenu] = useState(false);
@@ -156,6 +22,7 @@ const Home = () => {
       <div className={menu ? "ml-[20%]" : ""}>
         <Header toggleMenu={toggleMenu} />
         <NavBar />
+        <Headline />
         <div className="relative">
           <img src={bg} alt="Banner" className="w-full h-96 object-cover" />
           <div className="absolute inset-0 flex items-center justify-center">
