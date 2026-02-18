436ab61b1eb4db15be18509f6080ae9cbe0bb81d
Nikita Satish Madke <madkenikita35@gmail.com>
2026-02-18 13:57:37 +0530

    Add Styling to the Loader

--- ECommerceReact/src/Component/Loader.jsx
+++ ECommerceReact/src/Component/Loader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div className="h-5 w-5 bg-amber-800 rounded-full animate-bounce"></div>
+    <div className="justify-center sm:ml-90 md:ml-182  h-5 w-5 bg-amber-800 rounded-full animate-bounce"></div>
   );
 };
 
